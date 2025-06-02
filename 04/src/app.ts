5e7fcddbcfcad2fc069e7e8935b80c26848efd52
K1ng1nsu <96197161+K1ng1nsu@users.noreply.github.com>
2025-06-02 10:13:52 +0900

    d
    d

--- 04/src/app.ts
+++ 04/src/app.ts
@@ -10,27 +10,27 @@
 // 1. intersection types
 // interface로도 가능하다
 
-// 오브젝트를 intersection 하면 합친게 되는데 - 합집합
+// 오브젝트(type)를 intersection 하면 합친게 되는데 - 합집합
 // union type 을 intersection 하면 겹친것만 됨 - 교집합
 // c = a & b 가 있으면 a와 b를 모두 만족시키는 c가 나오는거
 
 // 합집합이 됨
 type Admin = {
-    name: string;
-    privileges: string[];
+  name: string;
+  privileges: string[];
 };
 
 type Employee = {
-    name: string;
-    startDate: Date;
+  name: string;
+  startDate: Date;
 };
 
 type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
-    name: 'insu',
-    privileges: ['create-server'],
-    startDate: new Date(),
+  name: 'insu',
+  privileges: ['create-server'],
+  startDate: new Date(),
 };
 
 // 교집합이 됨
@@ -44,43 +44,43 @@ type Universal = Combinable & Numeric;
 // 런타임 에러 방지, 컴파일러 자동완성 추론 기능 증대 이점이 있음
 
 function add(a: Combinable, b: Combinable) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        // 위에
-        return a.toString() + b.toString();
-    }
-    return a + b;
+  if (typeof a === 'string' || typeof b === 'string') {
+    // 위에
+    return a.toString() + b.toString();
+  }
+  return a + b;
 }
 
 type UnknownEmployee = Employee | Admin;
 
 function printEmployee(emp: UnknownEmployee) {
-    console.log(`Name: ${emp.name}`);
+  console.log(`Name: ${emp.name}`);
 
-    // console.log(`Privilegs: ${emp.privileges}`); -> error
+  // console.log(`Privilegs: ${emp.privileges}`); -> error
 
-    if ('privileges' in emp) {
-        console.log(`Privilegs: ${emp.privileges}`);
-    }
-    if ('startDate' in emp) {
-        console.log(`Start Date: ${emp.startDate}`);
-    }
+  if ('privileges' in emp) {
+    console.log(`Privilegs: ${emp.privileges}`);
+  }
+  if ('startDate' in emp) {
+    console.log(`Start Date: ${emp.startDate}`);
+  }
 }
 
 printEmployee({ name: 'insu', startDate: new Date() });
 
 class Car {
-    drive() {
-        console.log(`Driving...`);
-    }
+  drive() {
+    console.log(`Driving...`);
+  }
 }
 class Truck {
-    drive() {
-        console.log(`Driving a truck`);
-    }
+  drive() {
+    console.log(`Driving a truck`);
+  }
 
-    loadCargo(amount: number) {
-        console.log(`Loading cargo... ${amount}`);
-    }
+  loadCargo(amount: number) {
+    console.log(`Loading cargo... ${amount}`);
+  }
 }
 
 type Vehicle = Car | Truck;
@@ -89,12 +89,12 @@ const v1 = new Car();
 const v2 = new Truck();
 
 function useVehicle(vehicle: Vehicle) {
-    vehicle.drive();
+  vehicle.drive();
 
-    if (vehicle instanceof Truck) {
-        // instanceof
-        vehicle.loadCargo(1000);
-    }
+  if (vehicle instanceof Truck) {
+    // instanceof
+    vehicle.loadCargo(1000);
+  }
 }
 
 useVehicle(v1);
@@ -104,27 +104,27 @@ useVehicle(v2);
 // interface 로 Unions type을 하면 실제 클래스도 뭣도 아니라서 Type guard를 할 수 없는데
 // type 이나 kind 같은 프로퍼티를 추가시켜서 Type Guard를 사용하는 방법
 interface Bird {
-    type: 'bird';
-    flyingSpeed: number;
+  type: 'bird';
+  flyingSpeed: number;
 }
 interface Horse {
-    type: 'horse';
-    runningSpeed: number;
+  type: 'horse';
+  runningSpeed: number;
 }
 
 type Animal = Bird | Horse;
 
 function moveAnimal(animal: Animal) {
-    let speed;
-    switch (animal.type) {
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSpeed;
-            break;
-    }
-    console.log(`Moving apt speed: ${speed}`);
+  let speed;
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log(`Moving apt speed: ${speed}`);
 }
 
 moveAnimal({ type: 'bird', flyingSpeed: 100 });
@@ -139,7 +139,9 @@ const paragraph = document.querySelector('p');
 const paragraph2 = document.getElementById('message-output');
 // const paragraph2: HTMLElement | null
 
-const paragraph3 = document.getElementById('message-output') as HTMLInputElement;
+const paragraph3 = document.getElementById(
+  'message-output'
+) as HTMLInputElement;
 // const paragraph3: HTMLInputElement
 
 const paragraph4 = <HTMLInputElement>document.getElementById('message-output');
@@ -148,27 +150,27 @@ const paragraph4 = <HTMLInputElement>document.getElementById('message-output');
 // 5. index type
 // 컨테이너가 더 유연하면 좋겠다 싶을 때, type을 제한하면서
 interface ErrorContainer {
-    // { email: 'Not a valid email', username: 'Must start with a character'}
-    // prop의 이름도 아직 잘 모르겠고 몇 개가 올지도 모르겠지만
-    // prop은 string 이어야하고 value도 string 이어야 한다.
-    // [key: KeyType] : ValueType
-    // [key in type1 | type2 ] : string;
-    [prop: string]: string;
+  // { email: 'Not a valid email', username: 'Must start with a character'}
+  // prop의 이름도 아직 잘 모르겠고 몇 개가 올지도 모르겠지만
+  // prop은 string 이어야하고 value도 string 이어야 한다.
+  // [key: KeyType] : ValueType
+  // [key in type1 | type2 ] : string;
+  [prop: string]: string;
 }
 
 const errorBag: ErrorContainer = {
-    email: 'Not a Valid email!',
-    username: 'Must start with a capital character',
+  email: 'Not a Valid email!',
+  username: 'Must start with a capital character',
 };
 
 // 6. Function overload
 
 function add1(a: Combinable, b: Combinable) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        // 위에
-        return a.toString() + b.toString();
-    }
-    return a + b;
+  if (typeof a === 'string' || typeof b === 'string') {
+    // 위에
+    return a.toString() + b.toString();
+  }
+  return a + b;
 }
 
 const result = add1(1, 5); //as number;
@@ -178,12 +180,12 @@ const result = add1(1, 5); //as number;
 function add2(a: number, b: number): number;
 function add2(a: string, b: string): string; // 이 두개를 overload 시그니처라고 함
 function add2(a: Combinable, b: Combinable) {
-    // 실제 구현 부분 위의 모든 호출 방식의 타입을 포괄해야함
-    if (typeof a === 'string' || typeof b === 'string') {
-        // 위에
-        return a.toString() + b.toString();
-    }
-    return a + b;
+  // 실제 구현 부분 위의 모든 호출 방식의 타입을 포괄해야함
+  if (typeof a === 'string' || typeof b === 'string') {
+    // 위에
+    return a.toString() + b.toString();
+  }
+  return a + b;
 }
 
 const result2 = add2(1, 5); // 이러면 number를 받는다는걸 안다
@@ -191,11 +193,11 @@ const result2 = add2(1, 5); // 이러면 number를 받는다는걸 안다
 // 7. optional chaining
 
 const user = {
-    name: 'Alice',
-    address: {
-        city: 'Seoul',
-        postalCode: '12345',
-    },
+  name: 'Alice',
+  address: {
+    city: 'Seoul',
+    postalCode: '12345',
+  },
 };
 // null 또는 undefined 가 아닌 경우에만 해당 속성이나 함수를 호출하도록 함 -> http 통신등으로 받는걸 정확히 모르는 경우 유용
 console.log(user?.address?.city); // "Seoul"
